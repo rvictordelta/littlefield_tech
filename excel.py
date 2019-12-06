73a77123a03298336a9be7241619f4a1924f81d8
Rob <rvandusen@4170trading.com>
2019-12-06 09:48:16 -0600

    ordered sheets for sofi

--- excel.py
+++ excel.py
@@ -7,7 +7,10 @@ def consolidate_data_excel(Group):
     d = Group.id + '/'
     writer = pd.ExcelWriter(d + f'hourly_summary_{Group.id}.xlsx',
                             engine='xlsxwriter')
-    for filename in os.listdir(d):
+    # temp hack for Sofi's order of sheets
+    filename_list = ['standings.csv', 'jobs_leadTime.csv', 'jobs_avg_revenue_per_job.csv', 'jobs_completed.csv', 'inventory_level.csv', 'jobs_accepted.csv', 'sta1_queue.csv', 'sta1_util.csv', 'sta2_queue.csv', 'sta2_util.csv', 'sta3_queue.csv', 'sta3_util.csv']
+    # for filename in os.listdir(d):
+    for filename in filename_list:
         if ".xlsx" not in filename:
             try:
                 df = pd.read_csv(d + filename)
